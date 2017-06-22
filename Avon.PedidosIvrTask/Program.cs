d86570490b89214d1d41493c3eeaf1616f49db66
Rodrigo Velasco <rockdrigo.velasco@gmail.com>
2017-06-21 23:15:07 -0500

    Se agrega parametro para indicar el numero de pedidos por paquete

--- Avon.PedidosIvrTask/Program.cs
+++ Avon.PedidosIvrTask/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avon.PedidosIvr.Business.Services;
 using System;
+using System.Configuration;
 using System.Linq;
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
@@ -11,8 +12,14 @@ namespace Avon.PedidosIvrTask
         private static readonly log4net.ILog _log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         private static PedidosService _pedidosService = new PedidosService();
 
+
         static void Main(string[] args)
         {
+            //Numero de pedidos en cada paquete, valor por default
+            int pedidosPorPaquete = 30;
+
+            Int32.TryParse(ConfigurationManager.AppSettings["PedidosPorPaquete"], out pedidosPorPaquete);
+
             var grupos = _pedidosService.GeneraGrupos(1);
 
             _log.Info("Iniciando procesamiento de pedidos...");
